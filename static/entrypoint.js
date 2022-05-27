8838cc2cce3234d770f86aeab86d3ab3eade5175
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-27 22:43:23 +0430

    framework statics init

---
+++ static/entrypoint.js
@@ -0,0 +1,6 @@
+const go = new Go();
+WebAssembly.instantiateStreaming(fetch("lib.wasm"), go.importObject).then(
+    result => {
+        go.run(result.instance).then(r => console.log(r));
+    }
+);
