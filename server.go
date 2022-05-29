84ce0255040dc78205659be6aa75b7fe53ebb508
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-29 12:25:39 +0430

    update: server address refactor

--- server.go
+++ server.go
@@ -7,12 +7,13 @@ import (
 )
 
 var (
-	listen = flag.String("listen", ":8080", "listen address")
-	dir    = flag.String("dir", ".", "directory to serve")
+	address = flag.String("listen", ":8080", "listen address")
+	dir     = flag.String("dir", ".", "directory to serve")
 )
 
 func Run() {
 	flag.Parse()
-	log.Printf("listening on %q...", *listen)
-	log.Fatal(http.ListenAndServe(*listen, http.FileServer(http.Dir(*dir))))
+
+	log.Printf("listening on %q...", *address)
+	log.Fatal(http.ListenAndServe(*address, http.FileServer(http.Dir(*dir))))
 }
