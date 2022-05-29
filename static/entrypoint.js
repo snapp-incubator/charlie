0c5a7ad405ec46517d50e1fac8df9f10a6629c9c
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-29 12:11:04 +0430

    update: freameWork var name

--- static/entrypoint.js
+++ static/entrypoint.js
@@ -1,6 +1,12 @@
+// framework name
+const frameworkName = "lib.wasm";
+
+// creating a new go instance to create oir framework in lib.was
 const go = new Go();
-WebAssembly.instantiateStreaming(fetch("lib.wasm"), go.importObject).then(
+WebAssembly.instantiateStreaming(fetch(frameworkName), go.importObject).then(
     result => {
-        go.run(result.instance).then(r => console.log(r));
+        go.run(result.instance)
+            .then(r => console.log(r))
+            .catch(e => console.error(e))
     }
 );
