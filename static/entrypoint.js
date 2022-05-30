98115cbbb51b9de0a0424da36db43573b712d498
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-30 14:03:56 +0430

    entrypoint framework updated

--- static/entrypoint.js
+++ static/entrypoint.js
@@ -1,12 +1,18 @@
 // framework name
-const frameworkName = "lib.wasm";
+const frameworkName = "./../lib/lib.wasm";
 
 // creating a new go instance to create oir framework in lib.was
 const go = new Go();
-WebAssembly.instantiateStreaming(fetch(frameworkName), go.importObject).then(
-    result => {
+
+WebAssembly.instantiateStreaming(fetch(frameworkName), go.importObject)
+    .then(result => {
         go.run(result.instance)
             .then(r => console.log(r))
-            .catch(e => console.error(e))
-    }
-);
+            .catch(e => {
+                console.error(e)
+                console.log("Error for go run")
+            })
+    })
+    .catch(e => {
+        console.error(e)
+    })
