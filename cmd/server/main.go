bc819cb6652a9ec3cc3a9f1ff7a7ac1562dd070e
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-07 03:47:27 +0430

    feat: success function creating

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	err := http.ListenAndServe(":9090", http.FileServer(http.Dir("../../assets")))
+	err := http.ListenAndServe(":9090", http.FileServer(http.Dir("./assets")))
 	if err != nil {
 		fmt.Println("Failed to start server", err)
 	}
