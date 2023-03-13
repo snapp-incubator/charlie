f4d773746d3c050291d4730df55bb14f6a112c24
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-13 14:48:28 +0330

    add: api command

--- cmd/api.go
+++ cmd/api.go
@@ -1,1 +1,21 @@
 package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+type API struct{}
+
+func (a API) Command() *cobra.Command {
+	return &cobra.Command{
+		Use:   "api",
+		Short: "DJaaS api server",
+		Run: func(_ *cobra.Command, _ []string) {
+			a.main()
+		},
+	}
+}
+
+func (a API) main() {
+
+}
