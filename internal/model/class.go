ca5f1897eb373f35c30aa0337c28d51410be8e95
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-16 10:25:59 +0330

    add: class model

--- internal/model/class.go
+++ internal/model/class.go
@@ -1,5 +1,10 @@
 package model
 
+// Class model.
+// each class belongs to a user.
+// class has a limit for the users.
 type Class struct {
 	Base
+	UserID   uint `json:"user_id"`
+	Capacity int  `json:"capacity"`
 }
