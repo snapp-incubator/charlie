1fd8ae49e5aea7e2df42de0a16868765726983c1
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-16 11:05:38 +0330

    add: auth handler

--- internal/port/auth/handler.go
+++ internal/port/auth/handler.go
@@ -1,1 +1,23 @@
 package auth
+
+import (
+	"github.com/amirhnajafiz/DJaaS/internal/repository"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Handler struct {
+	Repository repository.Repository
+}
+
+func (h *Handler) Register(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h *Handler) Login(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h *Handler) Authenticate(ctx *fiber.Ctx) error {
+	return nil
+}
