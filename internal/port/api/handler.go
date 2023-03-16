2478baade5da24ac5bde11939ebd6b08f7f7f845
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-16 11:03:36 +0330

    add: api handler

--- internal/port/api/handler.go
+++ internal/port/api/handler.go
@@ -1,1 +1,23 @@
 package api
+
+import (
+	"github.com/amirhnajafiz/DJaaS/internal/repository"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Handler struct {
+	Repository repository.Repository
+}
+
+func (h *Handler) MakeSubmit(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h *Handler) GetQuestionSubmits(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h *Handler) GetQuestionSubmitsOfUser(ctx *fiber.Ctx) error {
+	return nil
+}
