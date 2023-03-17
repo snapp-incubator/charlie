5fa8f59d4a3b076c66f07cfa50cea50b11a80cbc
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-17 12:57:58 +0330

    add: processor port

--- internal/port/processor/processor.go
+++ internal/port/processor/processor.go
@@ -1,1 +1,21 @@
 package processor
+
+import (
+	"github.com/amirhnajafiz/DJaaS/internal/repository"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Processor struct {
+	Repository repository.Repository
+}
+
+func (p *Processor) RegisterHandler() fiber.Router {
+	app := fiber.New().Group("/processor")
+
+	app.Put("/:submit_id", func(ctx *fiber.Ctx) error {
+		return nil
+	})
+
+	return app
+}
