6d122d11017303323f11ba212b3a4c118f84c616
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-17 12:45:56 +0330

    add: api port

--- internal/port/api/handler.go
+++ internal/port/api/handler.go
@@ -10,6 +10,16 @@ type Handler struct {
 	Repository repository.Repository
 }
 
+func (h *Handler) HandlerRegister() fiber.Router {
+	app := fiber.New().Group("/api")
+
+	app.Post("/submit/:question_id", h.MakeSubmit)
+	app.Get("/submit/:question_id", h.GetQuestionSubmitsOfUser)
+	app.Get("/submit/:question_id/all", h.GetQuestionSubmits)
+
+	return app
+}
+
 func (h *Handler) MakeSubmit(ctx *fiber.Ctx) error {
 	return nil
 }
