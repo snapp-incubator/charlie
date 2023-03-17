016814f132d3f497d1106d1620e953de8595f7ab
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-17 13:13:27 +0330

    add: process cmd

--- cmd/processor.go
+++ cmd/processor.go
@@ -1,10 +1,16 @@
 package cmd
 
 import (
+	"log"
+
+	"github.com/amirhnajafiz/DJaaS/internal/port/processor"
+
 	"github.com/spf13/cobra"
 )
 
-type Processor struct{}
+type Processor struct {
+	Handler processor.Processor
+}
 
 func (p Processor) Command() *cobra.Command {
 	return &cobra.Command{
@@ -16,4 +22,14 @@ func (p Processor) Command() *cobra.Command {
 	}
 }
 
-func (p Processor) main() {}
+func (p Processor) main() {
+	app := p.Handler.RegisterHandler()
+
+	go func() {
+		if err := app.Listen(":8080"); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	p.Handler.Process()
+}
