6d984b6870b0b75cedaee42b8371724e27ae6b09
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-18 09:54:30 +0330

    add: task base method

--- pkg/task/task.go
+++ pkg/task/task.go
@@ -1,1 +1,10 @@
 package task
+
+type Task struct {
+	Inputs   map[string]interface{}
+	Function func(inputs map[string]interface{}) error
+}
+
+func (t Task) Execute() error {
+	return t.Function(t.Inputs)
+}
