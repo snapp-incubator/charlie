3fb020ff81a28193a3a1f1f85fbe09fdb3789b38
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-18 09:48:51 +0330

    add: koanf load

--- go.mod
+++ go.mod
@@ -32,6 +32,7 @@ require (
 	github.com/valyala/fasthttp v1.44.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.4.7 // indirect
 	gorm.io/gorm v1.24.6 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,4 +1,37 @@
 package config
 
+import (
+	"log"
+
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/providers/structs"
+)
+
 type Config struct {
+}
+
+// LoadConfigs returns the config struct.
+func LoadConfigs() Config {
+	var instance Config
+
+	k := koanf.New(".")
+
+	// load default
+	if err := k.Load(structs.Provider(Default(), "koanf"), nil); err != nil {
+		log.Printf("error loading deafult: %v\n", err)
+	}
+
+	// load configs file
+	if err := k.Load(file.Provider("config.yaml"), yaml.Parser()); err != nil {
+		log.Printf("error loading config.yaml file: %v\n", err)
+	}
+
+	// unmarshalling
+	if err := k.Unmarshal("", &instance); err != nil {
+		log.Printf("error unmarshalling config: %v\n", err)
+	}
+
+	return instance
 }
