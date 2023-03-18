6c9cda4dd8382062fed3f403532b3506a98113c5
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-18 21:07:21 +0330

    add: worker struct

--- pkg/worker/worker.go
+++ pkg/worker/worker.go
@@ -1,1 +1,23 @@
 package worker
+
+import (
+	"github.com/amirhnajafiz/DJaaS/pkg/task"
+)
+
+type Master struct {
+	Cfg     Config
+	Channel chan task.Task
+}
+
+type Slave struct {
+	Channel chan task.Task
+}
+
+func New(cfg Config) Master {
+	channel := make(chan task.Task)
+
+	return Master{
+		Cfg:     cfg,
+		Channel: channel,
+	}
+}
