6370e62e6c2e600c0346b36df905bdaca51ee44b
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-19 21:44:54 +0330

    fix: runtime

--- internal/component/runtime/runtime.go
+++ internal/component/runtime/runtime.go
@@ -1,13 +1,34 @@
 package runtime
 
 import (
+	"fmt"
 	"os/exec"
+	"strings"
+
+	"github.com/amirhnajafiz/DJaaS/internal/component/filesystem"
 )
 
 type Runtime struct {
-	Cfg Config
+	Cfg    Config
+	System filesystem.System
 }
 
-func Run() {
-	exec.Command("")
+func (r *Runtime) Process(operator, path, input string) error {
+	cmd := exec.Command(operator, path)
+	cmd.Stdin = strings.NewReader(input)
+
+	var out strings.Builder
+
+	cmd.Stdout = &out
+
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+
+	result := out.String()
+	result = strings.Trim(result, "\n")
+
+	fmt.Println(result == input)
+
+	return nil
 }
