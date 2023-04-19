22ad361f2e2aa96450127cc93bb367659ba95807
amirhnajafiz <najafizadeh21@gmail.com>
2023-04-19 17:08:51 +0330

    fix: base image

--- core/script.sh
+++ core/script.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-echo '[OK] cloning into' "$REPOSITORY_PATH"
-
-# clone into repository
-git clone "$REPOSITORY_PATH" || echo 'failed to clone' && exit
-
-# get inside repository
-cd "$REPOSITORY_PATH" || echo 'failed to enter into repository' && exit
-cd "$SCRIPT_PATH" || echo 'failed to find script directory' && exit
+echo '[OK] cloning into' "$REPOSITORY"
 
 # install dependencies
 pip install -r requirements.txt || echo 'failed to install requirements'
