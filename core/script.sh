bac0443a803c8250ef43d37e4bcd1af0ceb10717
amirhnajafiz <najafizadeh21@gmail.com>
2023-04-19 16:19:37 +0330

    add: core script

--- core/script.sh
+++ core/script.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+echo 'cloning into' "$REPOSITORY_PATH"
+
 # clone into repository
+git clone "$REPOSITORY_PATH"
 
 # get inside repository
+cd "$REPOSITORY_PATH" || echo 'failed to clone into repository' && exit
 
 # install dependencies
+pip install -r requirements.txt || echo 'failed to install requirements'
 
 # execute the script
+python script.py
