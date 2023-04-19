e295a0bdcaee1f9f7dafd4e1126eddbcffac38af
amirhnajafiz <najafizadeh21@gmail.com>
2023-04-19 16:34:57 +0330

    fix: core script bug

--- core/script.sh
+++ core/script.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-echo 'cloning into' "$REPOSITORY_PATH"
+echo '[OK] cloning into' "$REPOSITORY_PATH"
 
 # clone into repository
 git clone "$REPOSITORY_PATH"
 
 # get inside repository
 cd "$REPOSITORY_PATH" || echo 'failed to clone into repository' && exit
+cd "$SCRIPT_PATH" || echo 'failed to find script directory' && exit
 
 # install dependencies
 pip install -r requirements.txt || echo 'failed to install requirements'
+
+echo '[OK] install requirements!'
 
 # execute the script
 python script.py
