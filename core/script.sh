4bd4a1af09606e447f597314815b602d1e660079
amirhnajafiz <najafizadeh21@gmail.com>
2023-04-19 16:40:55 +0330

    fix: bugs of script

--- core/script.sh
+++ core/script.sh
@@ -3,10 +3,10 @@
 echo '[OK] cloning into' "$REPOSITORY_PATH"
 
 # clone into repository
-git clone "$REPOSITORY_PATH"
+git clone "$REPOSITORY_PATH" || echo 'failed to clone' && exit
 
 # get inside repository
-cd "$REPOSITORY_PATH" || echo 'failed to clone into repository' && exit
+cd "$REPOSITORY_PATH" || echo 'failed to enter into repository' && exit
 cd "$SCRIPT_PATH" || echo 'failed to find script directory' && exit
 
 # install dependencies
