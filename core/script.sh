4468b10f9c321d8cc3014e45e011b77cd009b4bb
amirhnajafiz <najafizadeh21@gmail.com>
2023-04-29 20:09:40 +0330

    update: core script

--- core/script.sh
+++ core/script.sh
@@ -2,6 +2,14 @@
 
 echo '[OK] cloning into' "$REPOSITORY"
 
+cd clone || echo '[Failed] mounting' && ecit
+
+# clone into repo
+git clone "$REPOSITORY"
+
+# get into repository in order to run the script
+cd "$DIRECTORY"/"$SCRIPT_PATH" || echo "[Failed] repo not found" && exit
+
 # install dependencies
 pip install -r requirements.txt || echo '[Failed] install requirements!'
 
