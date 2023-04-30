6f765d1d22d5e25679c4b46e9acc7da777f07ded
amirhnajafiz <najafizadeh21@gmail.com>
2023-04-30 12:51:44 +0330

    update: core script

--- core/script.sh
+++ core/script.sh
@@ -1,23 +1,36 @@
 #!/usr/bin/env bash
 
-echo '[OK] cloning into' "$REPOSITORY"
 
 cd clone || exit
 
 echo '[OK] into clone'
 
-# remove if exists
 rm -rf "$DIRECTORY"
 
-# clone into repo
-git clone "$REPOSITORY"
+git_address=""
+
+if [ "$HTTP_SECURE" == true ]
+then
+  git_address="https://";
+else
+  git_address="http://";
+fi
+
+if [ "$GIT_SECURE" == true ]
+then
+  git_address="${git_address}${GIT_USER}:${GIT_TOKEN}@"
+fi
+
+git_address="${git_address}${REPOSITORY}"
+
+echo '[OK] cloning into' "$git_address"
+
+git clone "$git_address"
 
 echo '[OK] repo cloned!'
 
-# get into repository in order to run the script
 cd "$DIRECTORY"/"$SCRIPT_PATH" || exit
 
 echo '[OK] now running!'
 
-# execute the script
 python script.py
